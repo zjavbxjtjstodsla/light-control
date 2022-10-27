520bf6d7366f078644ea9df0f8acc205087a5dce
zjavbxjtjstodsla <116775846+zjavbxjtjstodsla@users.noreply.github.com>
2022-10-27 11:23:45 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    TPBot.headlightColor(0xff0000)
+})
+input.onButtonPressed(Button.AB, function () {
+    TPBot.headlightColor(0x00ff00)
+})
+input.onButtonPressed(Button.B, function () {
+    TPBot.headlightColor(0x0000ff)
+})
+basic.showIcon(IconNames.Happy)
+TPBot.headlightColor(0x000000)
